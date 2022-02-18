d34207c9101a8c5b7a72648e3487cd9bc5faf422
shyatu <67035613+shyatu@users.noreply.github.com>
2022-02-18 09:50:56 +0800

    Add save() to handle fields are not input by user (Ex. highlighted)

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -21,3 +21,16 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)

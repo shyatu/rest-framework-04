b30f35f0669fd3b0ddd9f2688dcb918d92e5acd2
shyatu <67035613+shyatu@users.noreply.github.com>
2022-02-18 10:36:10 +0800

    Add ReadOnlyField "owner" to SnippetSerializer (owner info is passed from view SnippetList)

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,6 +5,8 @@ from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
